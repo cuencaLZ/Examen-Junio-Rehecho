52c50b8cd19d002ffba4c2cc8c5910d9f8b19afb
Lazarocp <lazaro.cuenca8@gmail.com>
2018-09-03 23:42:42 +0200

    trabajando en la ultima prueba test

--- Lazaro_Cuenca_Poyatos/src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoMultiple.java
+++ Lazaro_Cuenca_Poyatos/src/main/java/org/mvpigs/commandpattern/tratamientos/TratamientoPedidoMultiple.java
@@ -1,4 +1,46 @@
 package org.mvpigs.commandpattern.tratamientos;
 
-public class TratamientoPedidoMultiple {
+import java.util.Set;
+
+import org.mvpigs.commandpattern.interfaces.Pedido;
+import org.mvpigs.commandpattern.interfaces.TratamientoPedido;
+
+public class TratamientoPedidoMultiple implements TratamientoPedido {
+
+	private Set<Pedido> pedidos;
+	private int pesoTotal;
+	private int numBultos;
+
+	public TratamientoPedidoMultiple(Set<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
+	public boolean tratar() {
+		if (pesoTotal > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+	}
+
+	public void calcularTotalBultos() {
+		return pedidos.size();
+		
+	}
+
+	public Object getNumBultos() {
+		// TODO Apéndice de método generado automáticamente
+		return null;
+	}
+
+	public void calcularPesoTotal() {
+		for( Pedido pedido : this.pedidos){
+			this.pesoTotal = this.pesoTotal + pedido.peso();
+			
+			
+		}
+		
+	}
 }
